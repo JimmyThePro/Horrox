fd1d6958d2ac1f45f551b1a7ef8a62548ffe835e
Jimmy <jimmyjunttila@hotmail.com>
2022-12-13 21:07:08 +0100

    Add bg-image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           <li className="navLink">Series</li>
         </ul>
       </div>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-4 text-white">
         <FaSearch />
       </div>
     </header>
