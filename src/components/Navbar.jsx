a9d1ac826dd1638c292972ae980e90f9f5963c82
Jimmy <jimmyjunttila@hotmail.com>
2022-12-13 18:38:33 +0100

    Add a scroll-effect on header, using 'hooks'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,39 @@
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, []);
+
   return (
-    <header>
-        <div className='flex items-center space-x-2 md:space-x-10'>
-            <h1 className='text-red-600 text-4xl font-bold cursor-pointer object-contain'>HORROX</h1>
-            <ul className='hidden space-x-4 md:flex'>
-                <li className='navLink'>Movies</li>
-                <li className='navLink'>Series</li>
-            </ul>
-        </div>
-        <div className='flex items-center space-x-4'>
-            <FaSearch />
-        </div>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer object-contain">
+          HORROX
+        </h1>
+        <ul className="hidden space-x-4 md:flex">
+          <li className="navLink">Movies</li>
+          <li className="navLink">Series</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4">
+        <FaSearch />
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
