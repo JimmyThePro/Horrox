79ccfebb46a53dc291871b12c2061df49b0bfea4
Jimmy <jimmyjunttila@hotmail.com>
2022-12-14 10:33:33 +0100

    Create pages and add correct links in navbar with routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { FaSearch } from "react-icons/fa";
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -21,12 +22,12 @@ const Navbar = () => {
   return (
     <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
-        <h1 className="text-red-600 text-4xl font-bold cursor-pointer object-contain">
+        <NavLink to='/'> <h1 className="text-red-600 text-4xl font-bold cursor-pointer object-contain">
           HORROX
-        </h1>
+        </h1></NavLink>
         <ul className="hidden space-x-4 md:flex">
-          <li className="navLink">Movies</li>
-          <li className="navLink">Series</li>
+          <li className="navLink"><NavLink to='/movies'>Movies</NavLink></li>
+          <li className="navLink"><NavLink to='/series'>Series</NavLink></li>
         </ul>
       </div>
       <div className="flex items-center space-x-4 text-white">
