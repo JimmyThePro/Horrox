c49e38b5a4871eb87d886eb4fb06044200607a39
Jimmy <jimmyjunttila@hotmail.com>
2022-12-20 16:25:22 +0100

    Fix so'no movie-text' in recently viewed slider removes, if a movie appears in that array

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-[#270606e4] text-white flex justify-center items-center h-14 mt-5 border-t-2 border-gray-700">
-            <div>
+        <footer className="bg-[#270606e4] h-14 border-t-2 border-gray-700 mt-6">
+            <div className="text-center text-white flex justify-center py-3">
                 <p>© 2022 Horrox - All rights reserved.</p>
-                
             </div>
         </footer>
     );
